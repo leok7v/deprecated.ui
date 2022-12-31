c6ff96b05f1300f32d132a32d40e8cd6a75de368
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2022-12-30 23:26:29 -0800

    edit.paste()

--- samples/edit.c
+++ samples/edit.c
@@ -638,6 +638,238 @@ static void uic_edit_move_caret(uic_edit_t* e, const uic_edit_pg_t pg) {
     e->ui.invalidate(&e->ui);
 }
 
+static char* uic_edit_ensure(uic_edit_t* e, int32_t pn, int32_t bytes,
+        int32_t preserve) {
+    assert(bytes >= 0 && preserve <= bytes);
+    if (bytes <= e->para[pn].allocated) {
+        // enough memory already allocated - do nothing
+    } else if (e->para[pn].allocated > 0) {
+        assert(preserve <= e->para[pn].allocated);
+        uic_edit_reallocate(&e->para[pn].text, bytes, 1);
+        fatal_if_null(e->para[pn].text);
+        e->para[pn].allocated = bytes;
+    } else {
+        assert(e->para[pn].allocated == 0);
+        char* text = null;
+        uic_edit_allocate(&text, bytes, 1);
+        e->para[pn].allocated = bytes;
+        memcpy(text, e->para[pn].text, preserve);
+        e->para[pn].text = text;
+        e->para[pn].bytes = preserve;
+    }
+    return e->para[pn].text;
+}
+
+
+#define uic_clip_append(a, ab, mx, text, bytes) do {  \
+    int32_t ba = (bytes); /* bytes to append */       \
+    if (a != null) {                                  \
+        assert(ab <= mx);                             \
+        memcpy(a, text, ba);                          \
+        a += ba;                                      \
+    }                                                 \
+    ab += ba;                                         \
+} while (0)
+
+static uic_edit_pg_t uic_edit_op(uic_edit_t* e, bool cut,
+        uic_edit_pg_t from, uic_edit_pg_t to, char* text, int32_t* bytes) {
+    char* a = text; // append
+    int32_t ab = 0; // appended bytes
+    int32_t limit = bytes != null ? *bytes : 0; // max byes in text
+    uint64_t f = uic_edit_uint64(from.pn, from.gp);
+    uint64_t t = uic_edit_uint64(to.pn, to.gp);
+    if (f != t) {
+        uic_edit_dispose_paragraphs_layout(e);
+        if (f > t) { uint64_t swap = t; t = f; f = swap; }
+        int32_t pn0 = (int32_t)(f >> 32);
+        int32_t gp0 = (int32_t)(f);
+        int32_t pn1 = (int32_t)(t >> 32);
+        int32_t gp1 = (int32_t)(t);
+        if (pn1 == e->paragraphs) { // last empty paragraph
+            assert(gp1 == 0);
+            pn1 = e->paragraphs - 1;
+            gp1 = uic_edit_g2b(e->para[pn1].text, e->para[pn1].bytes, null);
+        }
+        const int32_t bytes0 = e->para[pn0].bytes;
+        char* s0 = e->para[pn0].text;
+        char* s1 = e->para[pn1].text;
+        // worst case estimate: same number of glyphs as bytes
+        int32_t* g2b0 = (int32_t*)stackalloc(bytes0 * sizeof(int32_t));
+        uic_edit_g2b(s0, bytes0, g2b0);
+        const int bp0 = gp0 == 0 ? 0 : g2b0[gp0 - 1];
+        if (pn0 == pn1) { // edit inside same paragraph
+            const int bp1 = gp1 == 0 ? 0 : g2b0[gp1 - 1];
+            uic_clip_append(a, ab, limit, s0 + bp0, bp1 - bp0);
+            if (cut) {
+                if (e->para[pn0].allocated == 0) {
+                    s0 = null;
+                    int32_t n = bytes0 - (bp1 - bp0);
+                    uic_edit_allocate(&s0, n, 1);
+                    memcpy(s0, e->para[pn0].text, bp0);
+                    e->para[pn0].text = s0;
+                    e->para[pn0].allocated = n;
+                }
+                memcpy(s0 + bp0, s1 + bp1, bytes0 - bp1);
+                e->para[pn0].bytes -= (bp1 - bp0);
+            }
+        } else {
+            uic_clip_append(a, ab, limit, s0 + bp0, bytes0 - bp0);
+            uic_clip_append(a, ab, limit, "\n", 1);
+            for (int i = pn0 + 1; i < pn1; i++) {
+                uic_clip_append(a, ab, limit, e->para[i].text, e->para[i].bytes);
+                uic_clip_append(a, ab, limit, "\n", 1);
+            }
+            const int32_t bytes1 = e->para[pn1].bytes;
+            int32_t* g2b1 = (int32_t*)stackalloc(bytes1 * sizeof(int32_t));
+            uic_edit_g2b(s1, bytes1, g2b1);
+            const int bp1 = gp1 == 0 ? 0 : g2b1[gp1 - 1];
+            uic_clip_append(a, ab, limit, s1, bp1);
+            if (cut) {
+                int32_t total = bp0 + bytes1 - bp1;
+                s0 = uic_edit_ensure(e, pn0, total, bp0);
+                memcpy(s0 + bp0, s1 + bp1, bytes1 - bp1);
+                e->para[pn0].bytes = bp0 + bytes1 - bp1;
+            }
+        }
+        int32_t deleted = cut ? pn1 - pn0 : 0;
+        if (deleted > 0) {
+            for (int i = pn0 + 1; i <= pn0 + deleted; i++) {
+                if (e->para[i].allocated > 0) { free(e->para[i].text); }
+            }
+            for (int i = pn0 + 1; i < e->paragraphs - deleted; i++) {
+                e->para[i] = e->para[i + deleted];
+            }
+            for (int i = e->paragraphs - deleted; i < e->paragraphs; i++) {
+                memset(&e->para[i], 0, sizeof(e->para[i]));
+            }
+        }
+        if (t == uic_edit_uint64(e->paragraphs, 0)) {
+            uic_clip_append(a, ab, limit, "\n", 1);
+        }
+        if (a != null) { assert(a == text + limit); }
+        e->paragraphs -= deleted;
+        from.pn = pn0;
+        from.gp = gp0;
+    } else {
+        from.pn = -1;
+        from.gp = -1;
+    }
+    if (bytes != null) { *bytes = ab; }
+    (void)unused(limit); // only in debug
+    return from;
+}
+
+static void uic_edit_insert_paragraph(uic_edit_t* e, int32_t pn) {
+    uic_edit_dispose_paragraphs_layout(e);
+    if (e->paragraphs + 1 > e->allocated / (int32_t)sizeof(uic_edit_para_t)) {
+        int32_t n = (e->paragraphs + 1) * 3 / 2; // 1.5 times
+        uic_edit_reallocate(&e->para, n, (int32_t)sizeof(uic_edit_para_t));
+        e->allocated = n * (int32_t)sizeof(uic_edit_para_t);
+    }
+    e->paragraphs++;
+    for (int i = e->paragraphs - 1; i > pn; i--) {
+        e->para[i] = e->para[i - 1];
+    }
+    uic_edit_para_t* p = &e->para[pn];
+    p->text = null; // ? "" TODO
+    p->bytes = 0;
+    p->glyphs = 0;
+    p->allocated = 0;
+    p->runs = 0;
+    p->run = null;
+}
+
+// uic_edit_insert_inline() inserts text (not containing \n paragraph
+// break inside a paragraph)
+
+static uic_edit_pg_t uic_edit_insert_inline(uic_edit_t* e, uic_edit_pg_t pg,
+        const char* text, int32_t bytes) {
+    assert(bytes > 0); (void)(void*)unused(strnchr);
+    assert(strnchr(text, bytes, '\n') == null,
+           "text \"%s\" must not contain \\n character.", text);
+    if (pg.pn == e->paragraphs) {
+        uic_edit_insert_paragraph(e, pg.pn);
+    }
+    const int32_t b = e->para[pg.pn].bytes;
+    char* s = e->para[pg.pn].text;
+    // worst case estimate: same number of glyphs as bytes
+    int32_t* g2b0 = (int32_t*)stackalloc(b * sizeof(int32_t));
+    uic_edit_g2b(s, b, g2b0);
+    const int bp = pg.gp == 0 ? 0 : g2b0[pg.gp - 1];
+    int32_t n = (b + bytes) * 3 / 2; // heuristics 1.5 times of total
+    if (e->para[pg.pn].allocated == 0) {
+        s = null;
+        uic_edit_allocate(&s, n, 1);
+        memcpy(s, e->para[pg.pn].text, b);
+        e->para[pg.pn].text = s;
+        e->para[pg.pn].allocated = n;
+    } else if (e->para[pg.pn].allocated < b + bytes) {
+        uic_edit_reallocate(&s, n, 1);
+        e->para[pg.pn].text = s;
+        e->para[pg.pn].allocated = n;
+    }
+    s = e->para[pg.pn].text;
+    memmove(s + bp + bytes, s + bp, b - bp); // make space
+    memcpy(s + bp, text, bytes);
+    e->para[pg.pn].bytes += bytes;
+    uic_edit_dispose_paragraphs_layout(e);
+    pg.gp = uic_edit_g2b(s, bp + bytes, null);
+    return pg;
+}
+
+static uic_edit_pg_t uic_edit_break_paragraph(uic_edit_t* e, uic_edit_pg_t pg) {
+    uic_edit_insert_paragraph(e, pg.pn + (pg.pn < e->paragraphs));
+    const int32_t bytes = e->para[pg.pn].bytes;
+    char* s = e->para[pg.pn].text;
+    // worst case estimate: same number of glyphs as bytes
+    int32_t* g2b0 = (int32_t*)stackalloc(bytes * sizeof(int32_t));
+    uic_edit_g2b(s, bytes, g2b0);
+    const int bp = pg.gp == 0 ? 0 : g2b0[pg.gp - 1];
+    uic_edit_pg_t next = {.pn = pg.pn + 1, .gp = 0};
+    if (bp < bytes) {
+        (void)uic_edit_insert_inline(e, next, s + bp, bytes - bp);
+    } else {
+        uic_edit_dispose_paragraphs_layout(e);
+    }
+    e->para[pg.pn].bytes = bp;
+    return next;
+}
+
+static void uic_edit_erase(uic_edit_t* e) {
+    const uic_edit_pg_t from = e->selection[0];
+    const uic_edit_pg_t to = e->selection[1];
+    uic_edit_pg_t pg = uic_edit_op(e, true, from, to, null, null);
+    if (pg.pn >= 0 && pg.gp >= 0) {
+        e->selection[0] = pg;
+        e->selection[1] = pg;
+        uic_edit_move_caret(e, pg);
+        e->ui.invalidate(&e->ui);
+    }
+}
+
+static void uic_edit_cut_copy(uic_edit_t* e, bool cut) {
+    const uic_edit_pg_t from = e->selection[0];
+    const uic_edit_pg_t to = e->selection[1];
+    int32_t n = 0; // bytes between from..to
+    uic_edit_op(e, false, from, to, null, &n);
+    if (n > 0) {
+        char* text = null;
+        uic_edit_allocate(&text, n + 1, 1);
+        uic_edit_pg_t pg = uic_edit_op(e, cut, from, to, text, &n);
+        if (cut && pg.pn >= 0 && pg.gp >= 0) {
+            e->selection[0] = pg;
+            e->selection[1] = pg;
+            uic_edit_move_caret(e, pg);
+        }
+        text[n] = 0; // make it zero terminated
+        clipboard.copy_text(text);
+//      traceln("cliipboard (%d bytes strlen()%d):\n%s", n, strlen(text), text);
+        assert(n == strlen(text), "n=%d strlen(cb)=%d cb=\"%s\"",
+               n, strlen(text), text);
+        free(text);
+    }
+}
+
 static void uic_edit_check_focus(uic_edit_t* e) {
     // focus is two stage afair: window can be focused or not
     // and single UI control inside window can have focus
@@ -888,6 +1120,13 @@ static void uic_edit_key_backspace(uic_edit_t* e) {
     e->erase(e);
 }
 
+static void uic_edit_key_enter(uic_edit_t* e) {
+    e->erase(e);
+    e->selection[1] = uic_edit_break_paragraph(e, e->selection[1]);
+    e->selection[0] = e->selection[1];
+    uic_edit_move_caret(e, e->selection[1]);
+}
+
 static void uic_edit_next_fuzz(uic_edit_t* e);
 
 static void uic_edit_key_pressed(uic_t* ui, int32_t key) {
@@ -917,7 +1156,7 @@ static void uic_edit_key_pressed(uic_t* ui, int32_t key) {
         } else if (key == virtual_keys.back) {
             uic_edit_key_backspace(e);
         } else if (key == virtual_keys.enter) {
-//          uic_edit_key_enter(e);
+            uic_edit_key_enter(e);
         } else if (key == virtual_keys.f5) {
             if (app.ctrl && app.shift && app.alt && e->fuzzer == null) {
                 e->fuzz(e); // start on Ctrl+Shift+Alt+F5
@@ -937,6 +1176,14 @@ static void uic_edit_keyboard(uic_t* unused(ui), int32_t ch) {
     if (ch == (char)('x' - 'a' + 1) && app.ctrl) { e->cut(e); }
     if (ch == (char)('c' - 'a' + 1) && app.ctrl) { e->copy(e); }
     if (ch == (char)('v' - 'a' + 1) && app.ctrl) { e->paste(e); }
+    if (0x20 <= ch) { // 0x20 space
+        int32_t bytes = uic_edit_glyph_bytes(ch % 0xFF);
+        e->erase(e); // remove selected text to be replaced by glyph
+        e->selection[1] = uic_edit_insert_inline(e,
+            e->selection[1], (char*)&ch, bytes);
+        e->selection[0] = e->selection[1];
+        uic_edit_move_caret(e, e->selection[1]);
+    }
     if (e->focused) {
         ui->invalidate(ui);
     }
@@ -950,161 +1197,6 @@ static bool uic_edit_message(uic_t* ui, int32_t unused(message),
     return false;
 }
 
-static char* uic_edit_ensure(uic_edit_t* e, int32_t pn, int32_t bytes,
-        int32_t preserve) {
-    assert(bytes >= 0 && preserve <= bytes);
-    if (bytes <= e->para[pn].allocated) {
-        // enough memory already allocated - do nothing
-    } else if (e->para[pn].allocated > 0) {
-        assert(preserve <= e->para[pn].allocated);
-        uic_edit_reallocate(&e->para[pn].text, bytes, 1);
-        fatal_if_null(e->para[pn].text);
-        e->para[pn].allocated = bytes;
-    } else {
-        assert(e->para[pn].allocated == 0);
-        char* text = null;
-        uic_edit_allocate(&text, bytes, 1);
-        e->para[pn].allocated = bytes;
-        memcpy(text, e->para[pn].text, preserve);
-        e->para[pn].text = text;
-        e->para[pn].bytes = preserve;
-    }
-    return e->para[pn].text;
-}
-
-#define uic_clip_append(a, ab, mx, text, bytes) do {  \
-    int32_t ba = (bytes); /* bytes to append */       \
-    if (a != null) {                                  \
-        assert(ab <= mx);                             \
-        memcpy(a, text, ba);                          \
-        a += ba;                                      \
-    }                                                 \
-    ab += ba;                                         \
-} while (0)
-
-static uic_edit_pg_t uic_edit_op(uic_edit_t* e, bool cut,
-        uic_edit_pg_t from, uic_edit_pg_t to, char* text, int32_t* bytes) {
-    char* a = text; // append
-    int32_t ab = 0; // appended bytes
-    int32_t limit = bytes != null ? *bytes : 0; // max byes in text
-    uint64_t f = uic_edit_uint64(from.pn, from.gp);
-    uint64_t t = uic_edit_uint64(to.pn, to.gp);
-    if (f != t) {
-        uic_edit_dispose_paragraphs_layout(e);
-        if (f > t) { uint64_t swap = t; t = f; f = swap; }
-        int32_t pn0 = (int32_t)(f >> 32);
-        int32_t gp0 = (int32_t)(f);
-        int32_t pn1 = (int32_t)(t >> 32);
-        int32_t gp1 = (int32_t)(t);
-        if (pn1 == e->paragraphs) { // last empty paragraph
-            assert(gp1 == 0);
-            pn1 = e->paragraphs - 1;
-            gp1 = uic_edit_g2b(e->para[pn1].text, e->para[pn1].bytes, null);
-        }
-        const int32_t bytes0 = e->para[pn0].bytes;
-        char* s0 = e->para[pn0].text;
-        char* s1 = e->para[pn1].text;
-        // worst case estimate: same number of glyphs as bytes
-        int32_t* g2b0 = (int32_t*)stackalloc(bytes0 * sizeof(int32_t));
-        uic_edit_g2b(s0, bytes0, g2b0);
-        const int bp0 = gp0 == 0 ? 0 : g2b0[gp0 - 1];
-        if (pn0 == pn1) { // edit inside same paragraph
-            const int bp1 = gp1 == 0 ? 0 : g2b0[gp1 - 1];
-            uic_clip_append(a, ab, limit, s0 + bp0, bp1 - bp0);
-            if (cut) {
-                if (e->para[pn0].allocated == 0) {
-                    s0 = null;
-                    int32_t n = bytes0 - (bp1 - bp0);
-                    uic_edit_allocate(&s0, n, 1);
-                    memcpy(s0, e->para[pn0].text, bp0);
-                    e->para[pn0].text = s0;
-                    e->para[pn0].allocated = n;
-                }
-                memcpy(s0 + bp0, s1 + bp1, bytes0 - bp1);
-                e->para[pn0].bytes -= (bp1 - bp0);
-            }
-        } else {
-            uic_clip_append(a, ab, limit, s0 + bp0, bytes0 - bp0);
-            uic_clip_append(a, ab, limit, "\n", 1);
-            for (int i = pn0 + 1; i < pn1; i++) {
-                uic_clip_append(a, ab, limit,
-                    e->para[i].text, e->para[i].bytes);
-                uic_clip_append(a, ab, limit, "\n", 1);
-            }
-            const int32_t bytes1 = e->para[pn1].bytes;
-            int32_t* g2b1 = (int32_t*)stackalloc(bytes1 * sizeof(int32_t));
-            uic_edit_g2b(s1, bytes1, g2b1);
-            const int bp1 = gp1 == 0 ? 0 : g2b1[gp1 - 1];
-            uic_clip_append(a, ab, limit, s1, bp1);
-            if (cut) {
-                int32_t total = bp0 + bytes1 - bp1;
-                s0 = uic_edit_ensure(e, pn0, total, bp0);
-                memcpy(s0 + bp0, s1 + bp1, bytes1 - bp1);
-                e->para[pn0].bytes = bp0 + bytes1 - bp1;
-            }
-        }
-        int32_t deleted = cut ? pn1 - pn0 : 0;
-        if (deleted > 0) {
-            for (int i = pn0 + 1; i <= pn0 + deleted; i++) {
-                if (e->para[i].allocated > 0) { free(e->para[i].text); }
-            }
-            for (int i = pn0 + 1; i < e->paragraphs - deleted; i++) {
-                e->para[i] = e->para[i + deleted];
-            }
-            for (int i = e->paragraphs - deleted; i < e->paragraphs; i++) {
-                memset(&e->para[i], 0, sizeof(e->para[i]));
-            }
-        }
-        if (t == uic_edit_uint64(e->paragraphs, 0)) {
-            uic_clip_append(a, ab, limit, "\n", 1);
-        }
-        if (a != null) { assert(a == text + limit); }
-        e->paragraphs -= deleted;
-        from.pn = pn0;
-        from.gp = gp0;
-    } else {
-        from.pn = -1;
-        from.gp = -1;
-    }
-    if (bytes != null) { *bytes = ab; }
-    return from;
-}
-
-static void uic_edit_erase(uic_edit_t* e) {
-    const uic_edit_pg_t from = e->selection[0];
-    const uic_edit_pg_t to = e->selection[1];
-    uic_edit_pg_t pg = uic_edit_op(e, true, from, to, null, null);
-    if (pg.pn >= 0 && pg.gp >= 0) {
-        e->selection[0] = pg;
-        e->selection[1] = pg;
-        uic_edit_move_caret(e, pg);
-        e->ui.invalidate(&e->ui);
-    }
-}
-
-static void uic_edit_cut_copy(uic_edit_t* e, bool cut) {
-    const uic_edit_pg_t from = e->selection[0];
-    const uic_edit_pg_t to = e->selection[1];
-    int32_t n = 0; // bytes between from..to
-    uic_edit_op(e, false, from, to, null, &n);
-    if (n > 0) {
-        char* text = null;
-        uic_edit_allocate(&text, n + 1, 1);
-        uic_edit_pg_t pg = uic_edit_op(e, cut, from, to, text, &n);
-        if (cut && pg.pn >= 0 && pg.gp >= 0) {
-            e->selection[0] = pg;
-            e->selection[1] = pg;
-            uic_edit_move_caret(e, pg);
-        }
-        text[n] = 0; // make it zero terminated
-        clipboard.copy_text(text);
-//      traceln("cliipboard (%d bytes strlen()%d):\n%s", n, strlen(text), text);
-        assert(n == strlen(text), "n=%d strlen(cb)=%d cb=\"%s\"",
-               n, strlen(text), text);
-        free(text);
-    }
-}
-
 static void uic_edit_cut(uic_edit_t* e) {
     uic_edit_cut_copy(e, true);
 }
@@ -1113,8 +1205,46 @@ static void uic_edit_copy(uic_edit_t* e) {
     uic_edit_cut_copy(e, false);
 }
 
+static void uic_edit_paste_text(uic_edit_t* e, const char* s, int32_t n) {
+    uic_edit_pg_t pg = e->selection[1];
+    int32_t i = 0;
+    const char* text = s;
+    while (i < n) {
+        int32_t b = i;
+        while (b < n && s[b] != '\n') { b++; }
+        bool lf = b < n && s[b] == '\n';
+        int32_t next = b + 1;
+        if (b > i && s[b - 1] == '\r') { b--; } // CR LF
+        if (b > i) {
+            pg = uic_edit_insert_inline(e, pg, text, b - i);
+        }
+        if (lf) {
+            pg = uic_edit_break_paragraph(e, pg);
+        }
+        text = s + next;
+        i = next;
+    }
+    uic_edit_move_caret(e, pg);
+}
+
 static void uic_edit_paste(uic_edit_t* e) {
     (void)e;
+    int32_t bytes = 0;
+    clipboard.text(null, &bytes);
+    if (bytes > 0) {
+        char* text = null;
+        uic_edit_allocate(&text, bytes, 1);
+        int r = clipboard.text(text, &bytes);
+        fatal_if_not_zero(r);
+        if (bytes > 0 && text[bytes - 1] == 0) {
+            bytes--; // clipboard includes zero terminator
+        }
+        if (bytes > 0) {
+            e->erase(e);
+            uic_edit_paste_text(e, text, bytes);
+        }
+        uic_edit_free(&text);
+    }
 }
 
 
@@ -1299,8 +1429,10 @@ static void uic_edit_fuzzer(void* p) {
             virtual_keys.pageup,
             virtual_keys.pagedw,
             virtual_keys.insert,
-            virtual_keys.del,
-            virtual_keys.back,
+// TODO: cut copy paste erase need special treatment in fuzzing
+//       otherwise text collapses to nothing pretty fast
+//          virtual_keys.del,
+//          virtual_keys.back,
             virtual_keys.enter,
             0
         };
